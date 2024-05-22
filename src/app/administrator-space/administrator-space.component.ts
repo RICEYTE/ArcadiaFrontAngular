81f37e2028c4c443b92231f5f4769f95e92fc674
CMRJ5421 <christophe.ceyte@orange.com>
2024-05-22 17:35:41 +0200

    init branch for administrator space

---
+++ src/app/administrator-space/administrator-space.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administrator-space',
+  standalone: true,
+  imports: [],
+  templateUrl: './administrator-space.component.html',
+  styleUrl: './administrator-space.component.css'
+})
+export class AdministratorSpaceComponent {
+
+}
